b1c30f0a643861049b36a03f208194697b32840e
Sven Pachnit <sven@bmonkeys.net>
2019-01-18 19:33:17 +0100

    Fix history (wouldn't populate when empty)

--- dist/client.js
+++ dist/client.js
@@ -362,7 +362,7 @@
     }
 
     append(cmd) {
-      if (cmd && this.log.length && this.log[0] !== cmd) {
+      if (cmd && (this.log.length && this.log[0] !== cmd)) {
         this.log.unshift(cmd);
       }
       while (this.log.length > this.opts.limit) {

d506e93da7382e22ec04a30881466aa94e2663f2
Sven Pachnit <sven@bmonkeys.net>
2019-06-05 18:29:20 +0200

    Actually respect --action parameter in bookmarklet command

--- dist/server/commands.js
+++ dist/server/commands.js
@@ -805,15 +805,16 @@
       return client.ack();
     }
     if (withNotification) {
-      script = "(function(b){n=Notification;x=function(a){h=\"%wsurl%\";s=\"https://statics.bmonkeys.net/img/rpcico/\";w=window;w.stwsb=w.stwsb||[];if(w.stwsc){if(w.stwsc.readyState!=1){w.stwsb.push(a)}else{w.stwsc.send(a)}}else{w.stwsb.push(\"rpc_client\");w.stwsb.push(a);w.stwsc=new WebSocket(h);w.stwsc.onmessage=function(m){j=JSON.parse(m.data);if(j.type==\"rpc_response\"){new n(\"SyncTube\",{body:j.data.message,icon:s+j.data.type+\".png\"})}};w.stwsc.onopen=function(){while(w.stwsb.length){w.stwsc.send(w.stwsb.shift())}};w.stwsc.onerror=function(){alert(\"stwscError: failed to connect to \"+h);console.error(arguments[0])};w.stwsc.onclose=function(){w.stwsc=null}}};if(n.permission===\"granted\"||n.permission===\"denied\"){x(b)}else{n.requestPermission().then(function(result){x(b)})}})(\"/rpc -k %key% -c %channel% play \"+window.location)";
+      script = "(function(b){n=Notification;x=function(a){h=\"%wsurl%\";s=\"https://statics.bmonkeys.net/img/rpcico/\";w=window;w.stwsb=w.stwsb||[];if(w.stwsc){if(w.stwsc.readyState!=1){w.stwsb.push(a)}else{w.stwsc.send(a)}}else{w.stwsb.push(\"rpc_client\");w.stwsb.push(a);w.stwsc=new WebSocket(h);w.stwsc.onmessage=function(m){j=JSON.parse(m.data);if(j.type==\"rpc_response\"){new n(\"SyncTube\",{body:j.data.message,icon:s+j.data.type+\".png\"})}};w.stwsc.onopen=function(){while(w.stwsb.length){w.stwsc.send(w.stwsb.shift())}};w.stwsc.onerror=function(){alert(\"stwscError: failed to connect to \"+h);console.error(arguments[0])};w.stwsc.onclose=function(){w.stwsc=null}}};if(n.permission===\"granted\"||n.permission===\"denied\"){x(b)}else{n.requestPermission().then(function(result){x(b)})}})(\"/rpc -k %key% -c %channel% %action% \"+window.location)";
     } else {
-      script = "(function(a){h=\"%wsurl%\";w=window;w.stwsb=w.stwsb||[];if(w.stwsc){if(w.stwsc.readyState!=1){w.stwsb.push(a)}else{w.stwsc.send(a)}}else{w.stwsb.push(\"rpc_client\");w.stwsb.push(a);w.stwsc=new WebSocket(h);w.stwsc.onopen=function(){while(w.stwsb.length){w.stwsc.send(w.stwsb.shift())}};w.stwsc.onerror=function(){alert(\"stwscError: failed to connect to \"+h);console.error(arguments[0])};w.stwsc.onclose=function(){w.stwsc=null}}})(\"/rpc -k %key% -c %channel% play \"+window.location)";
+      script = "(function(a){h=\"%wsurl%\";w=window;w.stwsb=w.stwsb||[];if(w.stwsc){if(w.stwsc.readyState!=1){w.stwsb.push(a)}else{w.stwsc.send(a)}}else{w.stwsb.push(\"rpc_client\");w.stwsb.push(a);w.stwsc=new WebSocket(h);w.stwsc.onopen=function(){while(w.stwsb.length){w.stwsc.send(w.stwsb.shift())}};w.stwsc.onerror=function(){alert(\"stwscError: failed to connect to \"+h);console.error(arguments[0])};w.stwsc.onclose=function(){w.stwsc=null}}})(\"/rpc -k %key% -c %channel% %action% \"+window.location)";
     }
     wsurl = client.request.origin.replace(/^https:\/\//, "wss://").replace(/^http:\/\//, "ws://");
     wsurl += `/${client.request.resourceURL.pathname}`;
     script = script.replace("%wsurl%", wsurl);
     script = script.replace("%channel%", this.name);
     script = script.replace("%key%", this.getRPCKey());
+    script = script.replace("%action%", desiredAction);
     client.sendSystemMessage(`The embedded key will change with the channel password!<br>\n<span style="color: ${COLORS.info}">Drag the following button to your bookmark bar:</span>\n<a href="javascript:${encodeURIComponent(script)}" class="btn btn-primary btn-xs" style="font-size: 10px">${label}</a>`, COLORS.warning);
     return client.ack();
   });

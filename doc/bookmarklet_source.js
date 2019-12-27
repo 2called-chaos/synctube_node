54cf8f19acb8024c693f77595fd90d3656c4a353
Sven Pachnit <sven@bmonkeys.net>
2019-12-27 22:45:19 +0100

    Update bookmarklet_source.js

--- doc/bookmarklet_source.js
+++ doc/bookmarklet_source.js
@@ -1,3 +1,6 @@
+//-----------------------------------------
+//--- Current video (with notification)
+//-----------------------------------------
 (function (b) {
   n = Notification;
   x = function (a) {
@@ -40,8 +43,10 @@
 })("/rpc -k %key% -c %channel% play " + window.location)
 
 
-
 //-----------------------------------------
+//--- Current video (no notification)
+//-----------------------------------------
+
 
 (function (a) {
   h = "%wsurl%";
@@ -67,3 +72,44 @@
     w.stwsc.onclose = function(){ w.stwsc = null }
   }
 })("/rpc -k %key% -c %channel% play " + window.location)
+
+
+//-----------------------------------------
+//--- Playlist
+//-----------------------------------------
+
+
+(function (a) {
+  h = "%wsurl%";
+  w = window;
+  w.stwsb = w.stwsb || [];
+
+  const nl = document.querySelectorAll("ytd-playlist-panel-renderer:not(.ytd-miniplayer) a.ytd-playlist-panel-video-renderer");
+  const idl = Array.prototype.map.call(nl, n => {
+    const m = n.href.match(/([A-Za-z0-9_\-]{11})/);
+    if(m){ return m[0] } else { return m }
+  }).filter(i=>!!i);
+
+  if(!confirm("Add "+idl.length+" videos?"))
+    return;
+
+  if (w.stwsc) {
+    if (w.stwsc.readyState != 1) {
+      idl.forEach(ytid => w.stwsb.push(a+ytid));
+    } else {
+      idl.forEach(ytid => w.stwsc.send(a+ytid));
+    }
+  } else {
+    w.stwsb.push("rpc_client");
+    idl.forEach(ytid => w.stwsb.push(a+ytid));
+    w.stwsc = new WebSocket(h);
+    w.stwsc.onopen = function(){
+      while (w.stwsb.length) { w.stwsc.send(w.stwsb.shift()) }
+    };
+    w.stwsc.onerror = function(){
+      alert("stwscError: failed to connect to " + h);
+      console.error(arguments[0])
+    };
+    w.stwsc.onclose = function(){ w.stwsc = null }
+  }
+})("/rpc -k %key% -c %channel% play ")

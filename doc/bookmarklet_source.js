35d19967dd2fd1b364b15102c0b5b455228e8ae0
Sven Pachnit <sven@bmonkeys.net>
2019-12-28 16:29:04 +0100

    Update bookmarklets to also support playlists
    until they change their layout :D

--- doc/bookmarklet_source.js
+++ doc/bookmarklet_source.js
@@ -1,5 +1,5 @@
 //-----------------------------------------
-//--- Current video (with notification)
+//--- Single/Playlist (with notification)
 //-----------------------------------------
 (function (b) {
   n = Notification;
@@ -7,16 +7,38 @@
     h = "%wsurl%";
     s = "https://statics.bmonkeys.net/img/rpcico/";
     w = window;
+    l = w.location.href;
     w.stwsb = w.stwsb || [];
+
+    const nl = document.querySelectorAll("ytd-playlist-panel-renderer:not(.ytd-miniplayer) a.ytd-playlist-panel-video-renderer");
+    const idl = Array.prototype.map.call(nl, n => {
+      const m = n.href.match(/([A-Za-z0-9_\-]{11})/);
+      if(m){ return m[0] } else { return m }
+    }).filter(i=>!!i);
+
+    if (idl.length) {
+      if(!confirm("Add "+idl.length+" videos from playlist?")) {
+        im = l.match(/([A-Za-z0-9_\-]{11})/);
+        if(im && confirm("Do you want to add the current video instead?\\nwatch?v="+im[0])) {
+          idl.length = 0;
+          idl.push(l)
+        } else {
+          return
+        }
+      }
+    } else {
+      idl.push(l)
+    }
+
     if (w.stwsc) {
       if (w.stwsc.readyState != 1) {
-        w.stwsb.push(a)
+        idl.forEach(ytid => w.stwsb.push(a+ytid));
       } else {
-        w.stwsc.send(a)
+        idl.forEach(ytid => w.stwsc.send(a+ytid));
       }
     } else {
       w.stwsb.push("rpc_client");
-      w.stwsb.push(a);
+      idl.forEach(ytid => w.stwsb.push(a+ytid));
       w.stwsc = new WebSocket(h);
       w.stwsc.onmessage = function(m){
         j = JSON.parse(m.data);
@@ -40,48 +62,17 @@
   } else {
     n.requestPermission().then(function(result) { x(b) })
   }
-})("/rpc -k %key% -c %channel% play " + window.location)
+})("/rpc -k %key% -c %channel% play ")
 
 
-//-----------------------------------------
-//--- Current video (no notification)
-//-----------------------------------------
-
-
-(function (a) {
-  h = "%wsurl%";
-  w = window;
-  w.stwsb = w.stwsb || [];
-  if (w.stwsc) {
-    if (w.stwsc.readyState != 1) {
-      w.stwsb.push(a)
-    } else {
-      w.stwsc.send(a)
-    }
-  } else {
-    w.stwsb.push("rpc_client");
-    w.stwsb.push(a);
-    w.stwsc = new WebSocket(h);
-    w.stwsc.onopen = function(){
-      while (w.stwsb.length) { w.stwsc.send(w.stwsb.shift()) }
-    };
-    w.stwsc.onerror = function(){
-      alert("stwscError: failed to connect to " + h);
-      console.error(arguments[0])
-    };
-    w.stwsc.onclose = function(){ w.stwsc = null }
-  }
-})("/rpc -k %key% -c %channel% play " + window.location)
-
-
-//-----------------------------------------
-//--- Playlist
-//-----------------------------------------
-
+//------------------------------------------
+//--- Single/Playlist (without notification)
+//------------------------------------------
 
 (function (a) {
   h = "%wsurl%";
   w = window;
+  l = w.location.href;
   w.stwsb = w.stwsb || [];
 
   const nl = document.querySelectorAll("ytd-playlist-panel-renderer:not(.ytd-miniplayer) a.ytd-playlist-panel-video-renderer");
@@ -90,8 +81,19 @@
     if(m){ return m[0] } else { return m }
   }).filter(i=>!!i);
 
-  if(!confirm("Add "+idl.length+" videos?"))
-    return;
+  if (idl.length) {
+    if(!confirm("Add "+idl.length+" videos from playlist?")) {
+      im = l.match(/([A-Za-z0-9_\-]{11})/);
+      if(im && confirm("Do you want to add the current video instead?\\nwatch?v="+im[0])) {
+        idl.length = 0;
+        idl.push(l)
+      } else {
+        return
+      }
+    }
+  } else {
+    idl.push(l)
+  }
 
   if (w.stwsc) {
     if (w.stwsc.readyState != 1) {
